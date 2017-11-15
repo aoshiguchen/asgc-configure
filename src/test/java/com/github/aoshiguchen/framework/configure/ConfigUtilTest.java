384e17a50788c3f89c275ed2ff8918317bd2823f
yangwen <1052045476@qq.com>
2017-11-15 10:15:19 +0800

    完善测试用例

--- src/test/java/com/github/aoshiguchen/framework/configure/ConfigUtilTest.java
+++ src/test/java/com/github/aoshiguchen/framework/configure/ConfigUtilTest.java
@@ -7,27 +7,38 @@ import com.github.aoshiguchen.framework.configure.annotation.Config;
 public class ConfigUtilTest {
 	
 	@Config
-	private int a;
-	@Config
-	private String b;
+	private String projectName;
 	
-	@Test
-	public void test1(){
-		Configure configure = ConfigReaderFactory.getConfigure("config");
-		
-		ConfigUtil.inject(configure, this);
-		
-		System.out.println(a);
-		System.out.println(b);
+	@Config("jdbc.port")
+	private int port;
+	
+	@Config("jdbc.url")
+	private String url;
+	
+	{
+		/**
+		 * 与spring整合时，可以将这行代码置入spring前置处理器
+		 * sprign容器初始化时就会自动注入
+		 */
+		ConfigUtil.inject("config", this);
 	}
 	
+	//注入式读取配置
+	@Test
+	public void test1(){
+		System.out.println(projectName);
+		System.out.println(port);
+		System.out.println(url);
+	}
+	
+	//直接读取
 	@Test
 	public void test2(){
-
-		ConfigUtil.inject("config", this);
+		Configure configure = ConfigReaderFactory.getConfigure("config");
 		
-		System.out.println(a);
-		System.out.println(b);
+		System.out.println(configure.getString("jdbc.host"));
+		System.out.println(configure.getString("jdbc.driver_class"));
+		System.out.println(configure.getString("jdbc.username"));
 	}
 	
 }
